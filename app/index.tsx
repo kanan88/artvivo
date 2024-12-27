95c916fcdb7e96fb0eb0adfd38effb148bf38fba
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-12-27 02:30:30 +0100

    implement routing folders and files

--- app/index.tsx
+++ app/index.tsx
@@ -8,8 +8,8 @@ const App = () => {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Artvivo!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to Profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
       </Link>
     </View>
   );
