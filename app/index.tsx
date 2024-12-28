e03e2cd5e1a0409eef07d2cd5ce6b16462dfaf7a
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-12-28 17:23:53 +0100

    implement onboarding page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,55 @@
-import { Text, View } from "react-native";
-import React from "react";
+import CustomButton from "@/components/CustomButton";
+import { images } from "@/constants";
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Artvivo!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logoSmall}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-white text-3xl font-bold text-center tracking-widest">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Artvivo</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 right-24"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation:embark on a journey of limitless
+            exploration with Artvivo{" "}
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 };
 
